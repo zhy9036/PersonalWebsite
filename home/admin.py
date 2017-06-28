e693e26ea5c5eb938606498e74a1bb0446c2fe8f
Yang Zhang <yang.zhang@expeditors.com>
2017-06-28 16:46:59 -0700

    database migrations

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import UserLoginInfo
+from .models import UserLoginInfo, Projects
 
 # Register your models here.
 admin.site.register(UserLoginInfo)
+admin.site.register(Projects)
--- home/models.py
+++ home/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class UserLoginInfo(models.Model):
     username = models.CharField(primary_key=True, max_length=20)
@@ -8,5 +9,15 @@ class UserLoginInfo(models.Model):
     def __str__(self):
         return self.username
 
+
 class Projects(models.Model):
-    username = models.CharField(max_length=20)
+    username = models.ForeignKey(UserLoginInfo)
+    projectName = models.CharField(max_length=20)
+    projectUrl = models.CharField(primary_key=True, max_length=1000)
+    projectToken = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.projectName
+
+class UserPerf(models.Model):
+    username = models.ForeignKey(UserLoginInfo)
